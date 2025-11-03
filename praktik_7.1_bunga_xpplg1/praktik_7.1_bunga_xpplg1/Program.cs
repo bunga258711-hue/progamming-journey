241d8688b5d9e96b879dcd871345b254db3431cd
bunga258711-hue <bunga258711@student.smkn1kandeman.sch.id>
2025-11-03 00:46:23 -0600

    Add files via upload

---
+++ praktik_7.1_bunga_xpplg1/praktik_7.1_bunga_xpplg1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace praktik_7._1_bunga_xpplg1
+{
+    internal class Program
+    {
+        static void Sapa()
+        {
+            Console.WriteLine("Hallo, selmaat datang di pembelajaran C#! ");
+            Console.WriteLine("Mari belajar tentang fungsi! ");
+        }
+
+        static void Main(string[] args)
+        {
+            Sapa();
+        }
+    }
+}
