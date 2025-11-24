8a8c55fccb15a5acedf5c1216dcdecc8ac705602
bunga258711-hue <bunga258711@student.smkn1kandeman.sch.id>
2025-11-23 18:36:25 -0600

    Add files via upload

---
+++ praktek_5.2_bunga_xpplg1/praktek_5.2_bunga_xpplg1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace praktek_5._2_bunga_xpplg1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Variabel untuk menyimpan hasil penjumalahan
+            int total = 0;
+
+            Console.WriteLine("Menghitung jumlah angka dari 1 sampai 5: ");
+
+            // Perulangan for dari 1 hingga 5
+            for (int i = 1; i <= 5; i++)
+            {
+                //Tambahkan nilai i saat ini ke variabel total
+                total = total + i;
+
+                Console.WriteLine("Menambahkan " + i + ", total semnatara = " + total);
+
+            }
+            Console.WriteLine("\nHasil akhir penjumlahan adalah: " + total);
+
+        }
+    }
+}
